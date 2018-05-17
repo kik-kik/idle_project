3cf3ad180b4532b1a0a57af2bd5b25089911c4a8
Kristofelek <kignasiak01@gmail.com>
2018-05-17 20:24:17 +0200

    Adding the current stage of the project to github

---
+++ Idle Project/Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+
+public class Camera_Movement : MonoBehaviour
+{
+
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+
+    [SerializeField] private Camera linkedCamera;
+    private BoxCollider2D boxCollider;
+
+    void Start()
+    {
+        boxCollider = GetComponentInParent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+ 
+        if (!Input.GetMouseButton(0)) return;
+ 
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(0, pos.y * dragSpeed, 0);
+
+        transform.Translate(move, Space.World);
+    }
+
+
+    private void LateUpdate()
+    {
+        float vertExtent = linkedCamera.orthographicSize;
+
+        Vector3 linkedCameraPos = linkedCamera.transform.position;
+        Bounds areaBounds = boxCollider.bounds; // can move to start?
+
+        float minYMovement = areaBounds.min.y + vertExtent;
+        float maxYMovement = areaBounds.max.y - vertExtent;
+
+        float clampedYBoundary = Mathf.Clamp(linkedCameraPos.y, minYMovement, maxYMovement);
+
+        linkedCamera.transform.position = new Vector3(linkedCameraPos.x, clampedYBoundary, linkedCameraPos.z);
+    }
+
+}
---
+++ Idle Project/Assets/Scripts/Character_Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Character_Movement : MonoBehaviour {
+
+    #region variables
+    [SerializeField] private bool move = false;
+    [SerializeField] private bool manager = false;
+
+    [Header("Speed")]
+    [SerializeField] private float movementSpeed = 1f;
+    [Range(1,3)]
+    [SerializeField] private float speedRate = 1f;
+    #endregion
+
+    private void OnMouseDown()
+    {
+        move = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (move || manager)
+        {
+            MoveCharacter();
+        }
+        
+    }
+
+    /// <summary>
+    /// This method that moves the target on the X axis.
+    /// </summary>
+    void MoveCharacter()
+    {
+        float translatePos = movementSpeed * speedRate * Time.deltaTime;
+        transform.Translate(translatePos, 0f, 0f);
+    }
+
+    /// <summary>
+    /// This method rotates the target by 180 degrees on the Y axis.
+    /// </summary>
+    void RotateCharacter()
+    {
+        // this line below will switch between negative and positive values -> change direction
+        //movementSpeed -= movementSpeed * 2f;
+        //transform.Rotate(0f, transform.eulerAngles.y + 180f, 0f);
+        transform.rotation *= Quaternion.Euler(0f, 180f, 0f);
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        RotateCharacter();
+    }
+}
